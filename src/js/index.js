85f226ff562160d26547a60f33058f228c84881f
lolauriss <lolauriss@gmail.com>
2019-08-08 14:41:14 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,24 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  const nameGenerator = (arr1, arr2, arr3) => {
+    let nameDomain = "";
+
+    for (let i in arr1) {
+      for (let j in arr2) {
+        for (let k in arr3) {
+          nameDomain += arr1[i] + arr2[j] + arr3[k] + ".com \n";
+        }
+      }
+    }
+    return nameDomain;
+  };
+
+  document
+    .getElementsByTagName("button")
+    .addEventListener("click", nameGenerator);
 };
